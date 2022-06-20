727f80f7ba4705a40bf69e400ac96a5013c0bdcb
fabrizioserial <fabrizio.serial@ing.austral.edu.ar>
2022-06-20 17:29:07 -0300

    fix: CORS? v:198.22.345.12

--- src/main/java/com/fabrizioserial/jibberjabber/config/WebMvcConfig.java
+++ src/main/java/com/fabrizioserial/jibberjabber/config/WebMvcConfig.java
@@ -2,13 +2,16 @@ package com.fabrizioserial.jibberjabber.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").exposedHeaders("*");
+        registry.addMapping("/**").allowedMethods("*").exposedHeaders("*").allowedOrigins("*");
     }
 }
