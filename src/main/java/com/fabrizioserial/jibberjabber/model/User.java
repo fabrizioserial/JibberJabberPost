d171b75f044616c703533e08a538f1a7c1ebdd7b
fabrizioserial <fabrizio.serial@ing.austral.edu.ar>
2022-07-19 17:06:43 -0300

    fix: keycloak? Fixeade

--- src/main/java/com/fabrizioserial/jibberjabber/model/User.java
+++ src/main/java/com/fabrizioserial/jibberjabber/model/User.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -23,6 +20,7 @@ public class User {
     @GeneratedValue
     private UUID id;
 
+    @Column(unique=true)
     private String username;
 
     @Builder.Default
